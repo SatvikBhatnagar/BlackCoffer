695703e2e36d4c001959d837bf5ab474ea666824
stoner69 <16363598+SatvikBhatnagar@users.noreply.github.com>
2023-10-22 04:50:02 +0530

    reading all the stop words

--- modules/cleaning_using_stop_words.py
+++ modules/cleaning_using_stop_words.py
@@ -4,6 +4,7 @@ from .file_locations import RawArticleFiles, StopWordsFiles
 
 def cleaning():
     raw_articles = RawArticleFiles()
+    stop_words = StopWordsFiles()
 
     for file_name in raw_articles:
         if file_name.endswith("-ERROR.txt"):
@@ -15,9 +16,14 @@ def cleaning():
                 lines = file.readlines()
                 title = lines[0].strip()  # saves the first line as the title
                 article_content = ''.join(lines[1:])  # save rest of the article content to process
-                print("Title:", title)
-                print("Article:", article_content)
+
+    for file_name in stop_words:
+        if file_name.endswith(".txt"):
+            file_path = os.path.join("files/StopWords", file_name)
+            with open(file_path, 'r', encoding='cp1252') as file: # the encoding of the file is cp1252
+                stop_word = file.read()  # this variable has all the Stop Words
+                print(stop_word)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # ensures that the file is executed if the script is run as the main program
     cleaning()
