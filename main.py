42aee68e4408931a0177de35be09a6a59068b4f9
stoner69 <16363598+SatvikBhatnagar@users.noreply.github.com>
2023-10-22 03:48:46 +0530

    data extraction successful

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ from modules import file_locations as fl
 from modules import data_extraction as de
 
 input_file = fl.input_file()  # reading and loading Input.xlsx
-de.data_(input_file)  # passing input file to the Data Extraction module
+
+# extracting data
+data_extraction = input("Do you want to extract articles? (Type 'y' to extract): ")
+if data_extraction == 'y':
+    de.data_(input_file)  # passing input file to the Data Extraction module
