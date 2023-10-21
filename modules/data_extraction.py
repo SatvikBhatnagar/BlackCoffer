10a296e0528cd154f632b83272b35ff621b6d5b0
stoner69 <16363598+SatvikBhatnagar@users.noreply.github.com>
2023-10-22 02:21:32 +0530

    extracting title and text from the webpages

--- modules/data_extraction.py
+++ modules/data_extraction.py
@@ -1,5 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
 def data_(df):
+    def data_extraction(URL_ID, URL):
+        r = requests.get(url=URL, headers=headers)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        title = soup.select("h1.entry-title")
+        title_text = title[0].get_text()
+        print(title_text)
+        divs = soup.select("div.td-post-content.tagdiv-type")
+        for div in divs:
+            #find all the <p> tags inside the current div
+            p_tags = div.find_all('p')
+            for p in p_tags:
+                text = p.get_text()
+                print(text)
+
+
+        return None
+
     for index, row in df.iterrows():
         URL_ID = row['URL_ID']
         URL = row['URL']
-        print(URL_ID, URL)
+        data_extraction(URL_ID, URL)
