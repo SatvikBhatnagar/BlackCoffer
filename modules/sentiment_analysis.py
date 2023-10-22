01a18f65a02e13209e8752112814b0978cfe0094
stoner69 <16363598+SatvikBhatnagar@users.noreply.github.com>
2023-10-22 18:38:45 +0530

    calculate negative score

--- modules/sentiment_analysis.py
+++ modules/sentiment_analysis.py
@@ -5,6 +5,7 @@ positive_words_directory = positive_words()
 negative_words_directory = negative_words()
 
 positive_score_dict = {}
+negative_score_dict = {}
 
 
 def positive_score_calculate():
@@ -22,5 +23,21 @@ def positive_score_calculate():
             positive_score_dict[url_id] = positive_score
 
 
+def negative_score_calculate():
+    """Reads positive words from text file and store them in a dictionary with URL as key and score as value"""
+    for file_name in negative_words_directory:
+        if file_name.endswith(".txt"):
+            file_path = os.path.join("files/output/negative_words_from_articles", file_name)
+            with open(file_path, 'r', ) as file:
+                words = file.readlines()
+                words = [word.rstrip('\n') for word in words]
+
+                negative_score = sum(1 for _ in words)  # calculates positive score
+            url_id_txt = file_path.split('/')[-1]
+            url_id = url_id_txt.split('.')[0]
+            negative_score_dict[url_id] = negative_score
+
+
 def sentiment_analysis():
     positive_score_calculate()
+    negative_score_calculate()
