725d3ae6f2aa28ed0ab65d7ad43f50db681ba550
stoner69 <16363598+SatvikBhatnagar@users.noreply.github.com>
2023-10-24 01:03:01 +0530

    minor change

--- modules/write_output_file.py
+++ modules/write_output_file.py
@@ -16,20 +16,11 @@ def write_output(input_file, output_file):
 
     for url_id, p_score in positive_score.items():
         if url_id in values.keys():
-            url_tuple = values[url_id]  # getting the existing tuple
-            # updated_tuple = (url_tuple[0], p_score)
-            # values[url_id] = updated_tuple
+
             values[url_id] = values[url_id] + (p_score,)
 
     for url_id, n_score in negative_score.items():
         if url_id in values.keys():
-            url_tuple = values[url_id]  # getting the existing tuple
-            updated_tuple = (url_tuple, p_score)
             values[url_id] = values[url_id] + (n_score,)
 
-    #
-    # positive_score_keys = positive_score.keys()
-    # values_keys = values.keys()
-    #
-    # print("Positive Score Keys:", positive_score_keys)
-    # print("Values Keys:", values_keys)
+    print(values)
