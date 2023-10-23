0013cf88410c086450c85e4cdbce9d3dafb9abed
stoner69 <16363598+SatvikBhatnagar@users.noreply.github.com>
2023-10-24 01:08:50 +0530

    appending all the variables to the tuple

--- modules/write_output_file.py
+++ modules/write_output_file.py
@@ -10,17 +10,15 @@ def write_output(input_file, output_file):
         values[url_id] = (url, )
 
     (positive_score, negative_score, polarity_score, subjectivity_score,
-     average_sentence_length, complex_word_count, percentage_of_complex_words,
-     fog_index, average_number_of_words_per_sentence, word_count,
+     average_sentence_length, percentage_of_complex_words,
+     fog_index, average_number_of_words_per_sentence, complex_word_count, word_count,
      syllable_count_per_word, personal_pronouns, average_word_length) = sentiment_analysis()
 
-    for url_id, p_score in positive_score.items():
-        if url_id in values.keys():
+    analysis_var = [positive_score, negative_score, polarity_score, subjectivity_score,
+     average_sentence_length, percentage_of_complex_words,
+     fog_index, average_number_of_words_per_sentence, complex_word_count, word_count,
+     syllable_count_per_word, personal_pronouns, average_word_length]
 
-            values[url_id] = values[url_id] + (p_score,)
-
-    for url_id, n_score in negative_score.items():
-        if url_id in values.keys():
-            values[url_id] = values[url_id] + (n_score,)
-
-    print(values)
+    for var in analysis_var:
+        for url_id, var_to_append in var.items():
+            values[url_id] = values[url_id] + (var_to_append,)
